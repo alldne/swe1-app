6ccf886c98b1847d2c2c57ae16725db31e07b3f9
Yonguk Jeong <yonguk@nyu.edu>
2019-10-11 12:16:52 -0400

    Fix errors from after running flake8

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,9 @@
-from django.http import Http404, HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
 
 
 class IndexView(generic.ListView):
